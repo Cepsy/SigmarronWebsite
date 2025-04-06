005c0c550bdf045f3d06dda4b84d86ebf994a47c
Cepsy <47183647+Cepsy@users.noreply.github.com>
2025-04-06 21:30:54 +0200

    Quartz sync: Apr 6, 2025, 9:30 PM

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // The base path for your site if you’re using a subdirectory in GitHub Pages
+  base: "/SigmarronWebsite",  // Set this to your repository name
+});
