391ae65324f6dd09f981acf29931efaa78cd8e60
Cepsy <47183647+Cepsy@users.noreply.github.com>
2025-07-28 18:44:49 +0200

    Quartz sync: Jul 28, 2025, 6:44 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -4,7 +4,15 @@ import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div>
+      <div id="quartz-body">{children}</div>
+      <i data-lucide="heart"></i>
+      <script>
+        lucide.createIcons(); // Initializes all icons on the page
+      </script>
+    </div>
+  )
 }
 
 Body.afterDOMLoaded = clipboardScript
