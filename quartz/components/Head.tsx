484c655583dc3f82f17a77e73d37d2f2beda882e
Cepsy <47183647+Cepsy@users.noreply.github.com>
2025-07-28 18:26:22 +0200

    Quartz sync: Jul 28, 2025, 6:26 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -38,14 +38,15 @@ export default (() => {
 
     return (
       <head>
-        <script>
-          console.log("✅ Custom head loaded!");
-        </script>
+
 
         <title>{title}</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
+            <script>
+              console.log("✅ Custom head loaded!");
+            </script>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
@@ -54,6 +55,9 @@ export default (() => {
             )}
           </>
         )}
+                <script>
+          console.log("✅ Custom head loaded!");
+        </script>
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
